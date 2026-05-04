86604e0e47c58f90f7c19250b80acfc71cfdb393
Akhilesh <surampalliakhilesh19@gmail.com>
2026-05-04 18:58:48 +0200

    Virtual env created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Akhilesh Gupta',
+    author_email="surampalliakhilesh19@gmail.com",
+    packages=find_packages()
+)
